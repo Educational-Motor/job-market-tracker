d5fa661e3e60d24eb4d2db1e7bc1de5f4b475d95
Karl <163776953+Educational-Motor@users.noreply.github.com>
2026-04-30 13:49:39 -0500

    Remove table insight

--- dashboard.py
+++ dashboard.py
@@ -49,7 +49,7 @@ df_role_filtered = df_role[(df_role["country"] == selected_country) & (df_role["
 df_salary_filtered = df_salary[(df_salary["country"] == selected_country) & (df_salary["category"] == selected_category)]
 df_skill_filtered = df_skill[(df_skill["country"] == selected_country) & (df_skill["category"] == selected_category)]
 
-st.dataframe(df_role_filtered)
+# st.dataframe(df_role_filtered)
 
 # Trend charts side by side.
 col1, col2 = st.columns(2)
